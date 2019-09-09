2db4d0c8d4d28ad377d73f5d45f82a1d7fde042b
Elijah Shadbolt <elijah.shadbolt@gmail.com>
2019-09-09 14:10:40 +1200

    eli first

---
+++ Assets/Elijah/PanelManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelManager : MonoBehaviour
+{
+    public PanelDuplicator duplicator;
+
+    public void Process((Vector3Int coords, GameObject go))
+    {
+
+    }
+}
---
+++ Assets/Elijah/PanelSlot.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelSlot : MonoBehaviour
+{
+    private void Start()
+    {
+        StartCoroutine(Wow());
+    }
+
+    IEnumerator Wow()
+    {
+        yield return new WaitForSeconds(2);
+    }
+}
