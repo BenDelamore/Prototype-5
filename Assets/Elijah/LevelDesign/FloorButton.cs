4d0bcfa1467cc4569856adc5ee0bcd7d6983f78f
Elijah Shadbolt <elijah.shadbolt@gmail.com>
2019-09-10 13:05:53 +1200

    floor buttons

---
+++ Assets/Elijah/LevelDesign/FloorButton.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using DG.Tweening;
+
+public class FloorButton : MonoBehaviour
+{
+    public PlayerButtonZone zone;
+    public ExtendingObject eo;
+
+    private void Awake()
+    {
+        zone.onPressed -= OnPressed;
+        zone.onPressed += OnPressed;
+
+        zone.onReleased -= OnReleased;
+        zone.onReleased += OnReleased;
+    }
+
+    private void OnPressed()
+    {
+        eo.Extend();
+    }
+
+    private void OnReleased()
+    {
+        eo.Retract();
+    }
+}
---
+++ Assets/Elijah/LevelDesign/WallFall.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallFall : MonoBehaviour
+{
+    public Rigidbody[] rbs;
+    public float force = 1000;
+    public float radius = 100;
+
+    private void Start()
+    {
+        foreach (var rb in rbs)
+        {
+            rb.isKinematic = false;
+            rb.AddExplosionForce(force, transform.position, radius);
+        }
+
+        Destroy(gameObject);
+    }
+}
