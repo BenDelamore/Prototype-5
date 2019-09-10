870b7c5d138a6acb676a3d8f1700449c62653582
Elijah Shadbolt <elijah.shadbolt@gmail.com>
2019-09-11 10:27:49 +1200

    final door open with keys

---
+++ Assets/Elijah/FinalDoor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class FinalDoor : MonoBehaviour
+{
+    private bool hasOpened = false;
+    public void Open()
+    {
+        if (!hasOpened)
+        {
+            hasOpened = true;
+            transform.DORotate(new Vector3(0, 90, 0), 3, RotateMode.LocalAxisAdd);
+        }
+    }
+}
