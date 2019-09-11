7919b0f6a39f1f70c55b78c4a85c10adc81ce6d3
Elijah Shadbolt <elijah.shadbolt@gmail.com>
2019-09-11 17:51:24 +1200

    keys smooth animation to holders

--- Assets/Elijah/KeyPickup.cs
+++ Assets/Elijah/KeyPickup.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public enum KeyType
 {
@@ -15,10 +16,21 @@ public class KeyPickup : MonoBehaviour
 {
     public PlayerKeyHolder currentHolder;
     public KeyType type = KeyType.Blue;
+    public float grabTime = 1;
 
     private void Start()
     {
         Debug.Assert(currentHolder, "Holder is null. Did you accidentally put a key in the scene? Only pedestals should spawn keys.", this);
         Debug.Assert(type != KeyType.None, "KeyType is None. Did you forget to use a key variant?", this);
+    }
+
+    public void OnPickedUp(PlayerKeyHolder holder)
+    {
+        currentHolder = holder;
+        transform.SetParent(holder.pos, true);
+        transform.DOKill();
+        var seq = DOTween.Sequence();
+        seq.Append(transform.DOLocalMove(Vector3.zero, grabTime));
+        seq.Insert(0, transform.DOLocalRotate(Vector3.zero, grabTime));
     }
 }
